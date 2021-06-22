1e4de055d384fa6ef12b2c7c4c9936b2ac9acceb
Sagar Das <65722439+Sagar60@users.noreply.github.com>
2021-06-22 14:25:16 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -4,5 +4,5 @@ const env = process.env.NODE_ENV || 'production';
 const config = require('./config.json');
 
 const configEnv = config[env];
-console.log(configEnv,config,env );
+console.log(configEnv,env );
 Object.keys(configEnv).forEach( key=> process.env[key] = configEnv[key]);
