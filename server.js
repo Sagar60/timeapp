afec729026f3002cb6ac57609cbf8f275bbc6a84
Sagar Das <65722439+Sagar60@users.noreply.github.com>
2021-06-27 11:01:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,76 +1,122 @@
-
-/*
-const app = require('./index');
-const http = require('http');
-
-// server start module
-const server = http.createServer(app);
-const port = 4000;
-
-server.listen( port, ()=>{
-    console.log(`server started at port:${port}`);
-});
-*/
+  
 const express = require('express');
+const path = require('path');
 const app = express();
-const http = require('http');
 const bodyParser = require('body-parser');
-const morgan = require('morgan');       // morgan use to see what api use toin client site and for how long it give response
 const cors = require('cors');
-const checkAuth = require('./controller/tokenverify');
 
-require('./config/config');
-require('./config/db');
-/*
-app.use(morgan('dev'));
-*/
+// const express = require('express');
+// const app = express();
+
+// static is used for any other folder need use in render files 
+
+// app.use(express.static(__dirname+ '/first'));		// dist/'file package name'
+
+// app.use(express.static(__dirname + '/first/html'))
+
+// app.get('/',(req,res)=>{
+//     res.send('Hello Sagar');
+// })
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
-
-// user sign up
-
-// app.use((req,res,next)=>{
-//     res.header('Access-Control-Allow-Origin','*')      // * ->for which browser u want to access to serve
-//     res.header('Access-Control-Allow-Header','*');
-//     if(req.method === 'OPTIONS'){
-//         res.header('Access-Control-Allow-Methods','PUT,POST,GET,PATCH,DELETE');
-//         res.status(200).json({});
-//     }
-//     next();     // important to give this
-// });
-
-// 
-/*
-const userService = require('./routes/users');
-const picCotroller = require('./controller/pic.controller');
-app.use('/upload', express.static('uploads'))
-app.use('/user',userService );
-app.use('/pics',picCotroller.pic_updateRouter);
-*/
-app.use('/servercheck',(req,res,next)=>{
-    console.log('hh');
-    res.json({
-        message: 'server started '
+app.use('/server',(rer,res,next)=>{
+    res.status(200).json({
+        message: 'hello server started'
     })
 })
-/*
-app.use((req,res,next)=>{
-    const error = new Error('There has a problem');
-    error.status = 404;
-    next(error);
+
+app.get('/cal',(req,res)=>{
+    let result = 0;
+    const q1 = parseInt(req.query.q1);
+    const q2 = parseInt(req.query.q2)
+    result = q1+q2;
+    res.send(`
+        <h3> Answer is ${result} </h3>
+    `
+    );
+});
+app.get('/cal/:op',(req,res,next)=>{
+	// for html
+	// app.set('views','view');
+	// app.set('view engine','html');
+
+	// app.use(express.static(__dirname + '/view'));
+	
+	// app.engine('html', require('ejs').renderFile);
+	// app.set('view engine', 'html');
+
+	// for pug
+	app.set('views','view');
+	//or app.set('views',__dirname +'/view');
+	app.set('view engine','pug');
+
+	let op = req.params.op;
+	if(!op){
+		return next();
+	}
+    let result = 0;
+    const q1 = parseInt(req.query.q1);
+    const q2 =  parseInt(req.query.q2);
+    
+	switch (op){
+		case 'add':
+			result = q1+q2;
+			break;
+		case 'sub':
+			result = q1-q2;
+			break;
+		case 'mul':
+			result = q1*q2;
+			break;
+		case 'div':
+			result = q1/q2;
+			break;
+		default:
+			result = q1+q2;
+			break;
+	}
+
+    // res.send(`<h4>Result is =  ${result}<h4>`);
+    res.render('test',{title: 'hello sagar', ip1: q1, ip2: q2, output: result,op: op});
+
+});
+app.post('/cal/:op',(req,res)=>{
+	app.set('views','view');
+	//or app.set('views',__dirname +'/view');
+	app.set('view engine','pug');
+
+	let result = 0;
+    const q1 = parseInt(req.body.q1);
+    const q2 =  parseInt(req.body.q2);
+    const op = req.params.op ;
+	switch (op){
+		case 'add':
+			result = q1+q2;
+			break;
+		case 'sub':
+			result = q1-q2;
+			break;
+		case 'mul':
+			result = q1*q2;
+			break;
+		case 'div':
+			result = q1/q2;
+			break;
+		default:
+			result = q1+q2;
+			break;
+	}
+	console.log(op);
+
+	res.render('index',{title: 'hi sagar', ip1: q1, ip2: q2, output: result,op: op});
+	// res.send(req.params);
 });
 
-app.use((error,req,res,next)=>{
-    res.status( error.status || 500 );
-    res.json({
-        error: {
-            message: error.message
-        }
-    })
-});
-*/
+// app.get('/*',function(req,res) {
+// 		res.sendFile(path.join(__dirname+'/first/')); 	//here also name change as per app name
+// });
+
 app.get('/*',function(req,res) {
 	res.sendFile( 'index.html',{root: __dirname+'/first/html'} ); 	//here also name change as per app name
 });
