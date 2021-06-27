c36f0956034e033808bed9f72428f5dfdf3d7863
Sagar Das <65722439+Sagar60@users.noreply.github.com>
2021-06-27 11:14:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ require('./config/db');
 // })
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 app.use(cors());
 
 const userService = require('./routes/users');
