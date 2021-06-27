d1018f7d65b6b23fa50418cd2ccb3c627d675271
Sagar Das <65722439+Sagar60@users.noreply.github.com>
2021-06-27 11:39:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const checkAuth = require('./controller/tokenverify');
 //require('./config/config');
 require('./config/db');
 
-
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors());
@@ -24,6 +23,21 @@ app.use('/server',(req,res,next)=>{
     console.log('hh');
     res.json({
         message: 'Hello! server has started'
+    })
+});
+
+app.use((req,res,next)=>{
+    const error = new Error('There has a problem');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error,req,res,next)=>{
+    res.status( error.status || 500 );
+    res.json({
+        error: {
+            message: error.message
+        }
     })
 });
 
