ef1267837c2345136c30caeabf440704296d3cbb
Sagar Das <65722439+Sagar60@users.noreply.github.com>
2021-06-27 10:52:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+/*
 const app = require('./index');
 const http = require('http');
 
@@ -7,4 +9,71 @@ const port = 4000;
 
 server.listen( port, ()=>{
     console.log(`server started at port:${port}`);
+});
+*/
+const express = require('express');
+const app = express();
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');       // morgan use to see what api use toin client site and for how long it give response
+const cors = require('cors');
+const checkAuth = require('./controller/tokenverify');
+
+require('./config/config');
+require('./config/db');
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// user sign up
+
+// app.use((req,res,next)=>{
+//     res.header('Access-Control-Allow-Origin','*')      // * ->for which browser u want to access to serve
+//     res.header('Access-Control-Allow-Header','*');
+//     if(req.method === 'OPTIONS'){
+//         res.header('Access-Control-Allow-Methods','PUT,POST,GET,PATCH,DELETE');
+//         res.status(200).json({});
+//     }
+//     next();     // important to give this
+// });
+
+// 
+/*
+const userService = require('./routes/users');
+const picCotroller = require('./controller/pic.controller');
+app.use('/upload', express.static('uploads'))
+app.use('/user',userService );
+app.use('/pics',picCotroller.pic_updateRouter);
+*/
+app.use('/servercheck',(req,res,next)=>{
+    console.log('hh');
+    res.json({
+        message: 'server started '
+    })
+})
+
+app.use((req,res,next)=>{
+    const error = new Error('There has a problem');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error,req,res,next)=>{
+    res.status( error.status || 500 );
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
+
+app.get('/*',function(req,res) {
+	res.sendFile( 'index.html',{root: __dirname+'/first/html'} ); 	//here also name change as per app name
+});
+
+app.listen(process.env.PORT || 8080,()=>{
+	console.log('server started at 8080');
 });
